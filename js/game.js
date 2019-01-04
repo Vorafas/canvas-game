2af0cccdbf32a4fe355ab5d00e3c6575b633415b
Elshad <safarov.elshad007@gmail.com>
2019-01-04 20:22:36 +0500

    Update objects

--- js/game.js
+++ js/game.js
@@ -36,3 +36,23 @@ var reset = function(){
 	monster.x = 32 + (Math.random() * (canvas.width - 64));
 	monster.y = 32 + (Math.random() * (canvas.height - 64));
 };
+
+
+var update = function(modifier){
+	if(38 in keysDown){
+		hero.y -= hero.speed * modifier;
+	}
+	if(40 in keysDown){
+		hero.y += hero.speed * modifier;
+	}
+	if(37 in keysDown){
+		hero.x -= hero.speed * modifier;
+	}
+	if(39 in keyDown){
+		hero.x += hero.speed * modifier;
+	}
+	if(hero.x <= (monster.x + 32) && monster.x <= (hero.x + 32) && hero.y <= (monster.y + 32) && monster.y <= (hero.y + 32)){
+		++monsterCaught;
+		reset();
+	}
+};
