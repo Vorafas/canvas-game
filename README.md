0297f7f7b71b5e3d43d93fb9a28457215a0a694e
Vorafas <safarov.en@yandex.ru>
2022-05-21 13:32:27 +0500

    Inital commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Canvas game  
+  
+Simple HTML5 Canvas game 
+
+## How to run
+
+First clone this repository.
+```
+$ git clone https://github.com/Vorafas/canvas-game.git
+```
+Then go to the folder
+```
+$ cd canvas-game
+```
---
+++ js/game.js
@@ -0,0 +1,129 @@
+// Create the canvas
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext('2d');
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+// Background image
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+	bgReady = true;
+};
+bgImage.src = "img/background.png";
+
+// Hero image
+var heroReady = false;
+var heroImage = new Image();
+heroImage.onload = function(){
+	heroReady = true;
+};
+heroImage.src = "img/hero.png";
+
+// Monster image
+var monsterReady = false;
+var monsterImage = new Image();
+monsterImage.onload = function(){
+	monsterReady = true;
+};
+monsterImage.src = "img/monster.png";
+
+// Game objects
+var hero = {
+	speed: 256,
+	x: 0,
+	y: 0
+};
+var monster = {
+	x: 0,
+	y: 0
+};
+var monsterCaught = 0;
+
+// Handle keyboard controls
+var keysDown = {};
+addEventListener('keydown', function(e){
+	keysDown[e.keyCode] = true;
+});
+addEventListener('keyup', function(e){
+	delete keysDown[e.keyCode];
+});
+
+
+// Reset the game when the player catches a monster
+var reset = function(){
+	hero.x = canvas.width / 2;
+	hero.y = canvas.height / 2;
+	// Throw the monster somewhere on the screen randomly
+	monster.x = 32 + (Math.random() * (canvas.width - 64));
+	monster.y = 32 + (Math.random() * (canvas.height - 64));
+};
+
+// Update game objects
+var update = function(modifier){
+	if(38 in keysDown){ // Player holding up
+		hero.y -= hero.speed * modifier;
+	}
+	if(40 in keysDown){ // Player holding down
+		hero.y += hero.speed * modifier;
+	}
+	if(37 in keysDown){ // Player holding left
+		hero.x -= hero.speed * modifier;
+	}
+	if(39 in keysDown){ // Player holding right
+		hero.x += hero.speed * modifier;
+	}
+	// Are they touching?
+	if(hero.x <= (monster.x + 32) && monster.x <= (hero.x + 32) && hero.y <= (monster.y + 32) && monster.y <= (hero.y + 32)){
+		++monsterCaught;
+		reset();
+	}
+	// Can not go beyond
+	if(hero.x >= 461){
+		hero.x = 15;
+	}
+	if(hero.x <= 14){
+		hero.x = 460;
+	}
+	if(hero.y <= 21){
+		hero.y = 422;
+	}
+	if(hero.y >= 423){
+		hero.y = 24;
+	}
+};
+
+// Draw everything
+var render = function(){
+	if(bgReady){
+		ctx.drawImage(bgImage, 0, 0);
+	}
+	if (heroReady) {
+		ctx.drawImage(heroImage, hero.x, hero.y);
+	}
+	if (monsterReady) {
+		ctx.drawImage(monsterImage, monster.x, monster.y);
+	}
+	// Score
+	ctx.fillStyle = 'rgb(250, 250, 250)';
+	ctx.font = '24px Roboto';
+	ctx.textAlign = 'left';
+	ctx.textBaseLine = 'top';
+	ctx.fillText("Monsters caught: " + monsterCaught, 32, 50);
+};
+
+// The main game loop
+var main = function(){
+	var now = Date.now();
+	var delta = now - then;
+	update(delta / 1000);
+	render();
+	then =  now;
+	// Request to do this again ASAP
+	requestAnimationFrame(main);
+};
+
+var then = Date.now();
+reset();
+main();
